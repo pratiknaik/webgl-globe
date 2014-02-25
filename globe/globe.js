b89623470cbf7cdbaa35943ce3fc84e0906e131a
Pratik Naik <pratiknaik3005@gmail.com>
2014-02-25 15:26:27 -0600

    Added globemail everything

--- globe/globe.js
+++ globe/globe.js
@@ -60,7 +60,7 @@ DAT.Globe = function(container, colorFn) {
         'varying vec3 vNormal;',
         'void main() {',
           'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
-          'gl_FragColor = vec4( 0.73, 0.16, 0.96, 1.0 ) * intensity;',
+          'gl_FragColor = vec4( 1.0, 0.5, 0.0, 1.0 ) * intensity;',
         '}'
       ].join('\n')
     }
